b6e19ad628ebb319ae7ff0b44ce02f177f05a6a5
Jerry Takou <vianneytakou@gmail.com>
2023-07-12 09:23:24 +0200

    auth

--- ruby/app/routes/health.rb
+++ ruby/app/routes/health.rb
@@ -1,8 +1,16 @@
+
+
 class HealthRoutes < Sinatra::Base
   #I will usually check if the token is correct but I have no user in db to check that
   get('/') do
     if !request.env['HTTP_AUTHORIZATION'].empty?
-      'App working OK'
+      decoded_string = Base64.decode64(request.env['HTTP_AUTHORIZATION'][6..]).force_encoding('UTF-8').split(':', 2)
+      if !decoded_string[0].eql?("applicant") || ! decoded_string[1].eql?("pass")
+        status 403
+      else
+        'App working OK'
+      end
+
     else
       status 403
     end
